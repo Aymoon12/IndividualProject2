da5243d35c4475ae28e8ea9502c9e7f9166565c9
Ayman Anba <aymoonanba45@gmail.com>
2024-03-27 19:24:17 -0400

    Worked more

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+#include <fstream>
 
 using namespace std;
 
@@ -14,15 +16,15 @@ public:
     {
         name[0]='\0';
         count = 0;
-        left = NULL;
-        right = NULL;
+        left = nullptr;
+        right = nullptr;
     }
     itemNode(char itemName[], int population)
     {
         strcpy(name, itemName);
         count = population;
-        left = NULL;
-        right = NULL;
+        left = nullptr;
+        right = nullptr;
     }
 };
 
@@ -38,16 +40,16 @@ public:
     treeNameNode()
     {
         treeName[0]='\0';
-        theTree = NULL;
-        left = NULL;
-        right = NULL;
+        theTree = nullptr;
+        left = nullptr;
+        right = nullptr;
     }
     treeNameNode(char name[])
     {
         strcpy(treeName, name);
-        theTree = NULL;
-        left = NULL;
-        right = NULL;
+        theTree = nullptr;
+        left = nullptr;
+        right = nullptr;
     }
 };
 
@@ -58,26 +60,38 @@ public:
     treeName() : root(nullptr) {}
 
     //always checks if string1 is greater than string two
-    //i.e if it returns false then string 2 is bigger, but if it returns true then string1 is bigger
+    //i.e. if it returns false if string2 is bigger, but if it returns true then string1 is bigger
     bool compareStrings(const string string1, const string string2){
 
         char sone;
         char stwo;
         int i = 0;
-        bool answer = true;
+
+        bool answer;
+        if(string1.length() < string2.length())
+            answer = false;
+        else
+            answer = true;
         do{
             sone = string1[i];
             stwo = string2[i];
             i++;
             if(sone > stwo){
-                return answer;
+                return true;
             }
-            if(sone < stwo){
+            else if(sone < stwo){
                 return false;
             }
+            else
+                continue;
 
-        } while(sone != '\0' && stwo != '0')
+
+        } while(string1[i++] != '\0' && string2[i++] != '\0');
+
+        return answer;
+
     }
+    //searches if an item exists
     void search(const string type,const string item){
 
         treeNameNode* tree = findTreeName(type,root);
@@ -92,7 +106,7 @@ public:
         }
         cout<<itemN->name<<": count: "<<itemN->count<<endl;
     }
-
+    //searches for a NameTree
     treeNameNode* findTreeName(const string type, treeNameNode* roo) {
         if(roo->treeName == type){
             return roo;
@@ -108,7 +122,7 @@ public:
         }
         return nullptr;
     }
-
+    //Searches for an item using the root of the nameTree
     itemNode* findItem(const string item, itemNode* next){
         if(next->name == item){
             return next;
@@ -125,38 +139,100 @@ public:
         }
         return nullptr;
     }
-    void item_before(const string type, const string item){
-        treeNameNode* treeNode = findTreeName(type,root);
-        if(treeNode != nullptr){
-            itemNode* itemnode = findItem(item,treeNode->theTree);
-            if(itemnode != nullptr){
-                itemNode* itemTree = treeNode->theTree;
-                while(itemTree)
-                if(itemTree->name[0] > itemnode->name[0]){
-                    itemTree = itemTree->left;
-                }
-                if(itemTree->name[0] < itemnode->name[0]){
-                    itemTree = itemTree->right;
-                }
+    //Inorder traversal of the item trees
+    void inOrder(itemNode* root, string* arr ,int index){
+        if(root == nullptr){
+            return;
+        }
+        inOrder(root->left,arr,index);
+        arr[index] = root->name;
+        index++;
+        inOrder(root->right,arr,index);
+    }
 
+    //converts the item trees into an array with all the names
+    string* itemTreeToArray(itemNode* root){
+        string* itemArray = new string[11];
+        inOrder(root,itemArray,0);
+        return itemArray;
 
+    }
+    //prints all the items that come alphabetically before the item inserted
+    void item_before(const string type, const string item) {
+        treeNameNode *treeNode = findTreeName(type, root);
+        if (treeNode != nullptr) {
+            itemNode *itemnode = findItem(item, treeNode->theTree);
+            if (itemnode != nullptr) {
+                string* itemArray = itemTreeToArray(itemnode);
+                for(int i =0; i<itemArray->length();i++){
+                    if(itemnode->name > itemArray[i]){
+                        cout<<itemArray[i]<< " ";
+                    }
+                }
             }
             else
                 return;
+            } else
+                return;
+
+
         }
-        else
+
+    void inOrderCounting(itemNode* root,int index,int* count){
+        if(root == nullptr){
             return;
+        }
+        inOrderCounting(root->left,index,count);
+        count[index] += root->count;
+        index++;
+        inOrderCounting(root->right,index,count);
+    }
 
-
+    void countArray(itemNode* root){
+        int* count = new int[11];
+        inOrderCounting(root,0,count);
+        int index = 0;
+        int total = 0;
+        while(true){
+            cout<<count[index] <<" ";
+            total += count[index];
+            index++;
+            if(index < 11 || count[index] < 1000){
+                cout<<"+ ";
+            }
+            else {
+                cout << "= ";
+                break;
+            }
+        }
+        cout<<total<<endl;
 
 
     }
 
 
-
 };
 
 int main() {
+
+    ifstream fin;
+    string line;
+    string treeNames;
+    string totalItems;
+    string queries;
+    int readingLine = 1;
+
+    fin.open("in.txt");
+    while(fin){
+        getline(fin,line);
+        if(readingLine == 1){
+            size_t spacepos1 = line.find(' ');
+            treeNames = line.substr(0,spacepos1);
+            string line2 = line.substr(spacepos1,line.length());
+            totalItems = line.substr(spacepos1,line)
+        }
+    }
+    fin.close();
 
 
     return 0;
