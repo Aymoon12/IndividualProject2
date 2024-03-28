56412db0a248874a08036209b96109761bd9c86a
Ayman <aymoonanba45@gmail.com>
2024-03-28 17:39:29 -0400

    Added more functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
@@ -14,14 +15,14 @@ public:
 
     itemNode()
     {
-        name[0]='\0';
+        name[0] ='\0';
         count = 0;
         left = nullptr;
         right = nullptr;
     }
     itemNode(char itemName[], int population)
     {
-        strcpy(name, itemName);
+        strcpy(name,itemName);
         count = population;
         left = nullptr;
         right = nullptr;
@@ -39,14 +40,14 @@ public:
 
     treeNameNode()
     {
-        treeName[0]='\0';
+        treeName[0] ='\0';
         theTree = nullptr;
         left = nullptr;
         right = nullptr;
     }
     treeNameNode(char name[])
     {
-        strcpy(treeName, name);
+        strcpy(treeName,name);
         theTree = nullptr;
         left = nullptr;
         right = nullptr;
@@ -92,7 +93,7 @@ public:
 
     }
     //searches if an item exists
-    void search(const string type,const string item){
+    void search(const char type[],const string item){
 
         treeNameNode* tree = findTreeName(type,root);
         if(tree == nullptr){
@@ -107,7 +108,7 @@ public:
         cout<<itemN->name<<": count: "<<itemN->count<<endl;
     }
     //searches for a NameTree
-    treeNameNode* findTreeName(const string type, treeNameNode* roo) {
+    treeNameNode* findTreeName(const char type[], treeNameNode* roo) {
         if(roo->treeName == type){
             return roo;
         }
@@ -158,7 +159,7 @@ public:
 
     }
     //prints all the items that come alphabetically before the item inserted
-    void item_before(const string type, const string item) {
+    void item_before(const char type[], const string item) {
         treeNameNode *treeNode = findTreeName(type, root);
         if (treeNode != nullptr) {
             itemNode *itemnode = findItem(item, treeNode->theTree);
@@ -211,27 +212,149 @@ public:
     }
 
 
+
+
 };
+
+void createTreeName(treeName* Tree, char treetype[]){
+    treeNameNode* mainTree = Tree->root;
+    if(mainTree == nullptr){
+        Tree->root = new treeNameNode(treetype);
+        printf("Added %s to the tree\n",treetype);
+        return;
+    }
+    while (mainTree != nullptr) {
+        // Check if the current node's tree name is greater than the given tree type
+        bool rootBigger = strcmp(mainTree->treeName, treetype) > 0;
+
+        // If the tree name should go to the left subtree
+        if (rootBigger) {
+            // If left child is null, insert the node here
+            if (mainTree->left == nullptr) {
+                mainTree->left = new treeNameNode(treetype);
+                printf("Added %s to the tree\n", treetype);
+                return;
+            }
+            // Move to the left subtree
+            mainTree = mainTree->left;
+        } else { // If the tree name should go to the right subtree
+            // If right child is null, insert the node here
+            if (mainTree->right == nullptr) {
+                mainTree->right = new treeNameNode(treetype);
+                printf("Added %s to the tree\n", treetype);
+                return;
+            }
+            // Move to the right subtree
+            mainTree = mainTree->right;
+        }
+    }
+
+}
+
+
+
+
+void inOrder(treeNameNode* root){
+    if(root == nullptr){
+        return;
+    }
+    inOrder(root->left);
+    printf("%s tree \n", root->treeName);
+    inOrder(root->right);
+}
+void treeinOrder(treeName* root){
+    inOrder(root->root);
+}
+
+void addItem(treeName* Tree, char type[],char item[],int count){
+    treeNameNode* treeType = Tree->findTreeName(type,Tree->root);
+    itemNode* itemTree = treeType->theTree;
+    if(itemTree == nullptr){
+        treeType->theTree = new itemNode(item,count);
+        printf("Root added successfully\n");
+        return;
+    }
+    while(itemTree){
+
+        bool isBigger = strcmp(itemTree->name,item) > 0;
+        if(isBigger){
+
+            if(itemTree->left == nullptr){
+                itemTree->left = new itemNode(item,count);
+                printf("Added %s to the item tree",item);
+                return;
+            }
+            itemTree = itemTree->left;
+        }
+        else{
+            if(itemTree->right == nullptr){
+                itemTree->right = new itemNode(item,count);
+                printf("Added %s to the item tree", item);
+                return;
+            }
+            itemTree = itemTree->right;
+        }
+
+    }
+
+}
+
 
 int main() {
 
     ifstream fin;
     string line;
-    string treeNames;
-    string totalItems;
-    string queries;
+    int treeNames;
+    int totalItems;
+    int queries;
+    char word1[50];
+    char word2[50];
+    int count;
     int readingLine = 1;
+    auto* Tree = new treeName();
+    int n=0;
+    int i=0;
+    int q=0;
+    FILE* infile = fopen("in.txt", "r");
+    fscanf(infile, "%d %d %d", &treeNames, &totalItems, &queries);
+    cout<<treeNames<<" "<<totalItems<<" "<<queries<<endl;
+    while(n < treeNames){
+        fscanf(infile,"%s",word1);
+        createTreeName(Tree,word1);
+        n++;
+    }
+    while(i<totalItems){
+        fscanf(infile, "%s %s %d",word1,word2,&count);
+        printf("%d",count);
+        addItem(Tree,word1,word2,count);
+        i++;
+    }
 
-    fin.open("in.txt");
-    while(fin){
-        getline(fin,line);
-        if(readingLine == 1){
-            size_t spacepos1 = line.find(' ');
-            treeNames = line.substr(0,spacepos1);
-            string line2 = line.substr(spacepos1,line.length());
-            totalItems = line.substr(spacepos1,line)
+   /* while(!fin.eof()) {
+        getline(fin, line);
+        size_t spacepos1 = line.find(' ');
+        string line2 = line.substr(spacepos1 + 1, line.length());
+        size_t spacepos2 = line2.find(' ');
+        string line3 = line2.substr(spacepos2 + 1, line.length());
+        string firstWord = line.substr(0, spacepos1);
+        string secondWord = line2.substr(0, spacepos2);
+        string thirdWord = line2.substr(spacepos2 + 1, line2.length());
+        if (readingLine == 1) {
+            treeNames = firstWord;
+            totalItems = secondWord;
+            queries = thirdWord;
+            readingLine++;
+        }
+        else {
+            cout << firstWord << " " << secondWord << endl;
+            createTreeName(Tree, firstWord);
         }
     }
+
+    */
+
+    //cout<<Tree->root->right->treeName;
+    delete Tree;
     fin.close();
 
 
